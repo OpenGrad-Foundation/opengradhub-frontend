0946d59190ab44e9cfae82744db26b497a084a91
Suraj Sunil <surajsunil3369@gmail.com>
2026-07-24 21:18:06 +0530

    feat(courses): collaborators panel on course settings tab

--- app/dashboard/course-management/[id]/page.tsx
+++ app/dashboard/course-management/[id]/page.tsx
@@ -6,6 +6,7 @@ import { BackLink } from "@/components/back-link";
 import { useParams, useRouter, useSearchParams } from "next/navigation";
 import { useCurrentUrl } from "@/lib/useCurrentUrl";
 import CourseCurriculumEditor from "../_components/CourseCurriculumEditor";
+import CollaboratorsPanel from "./collaborators-panel";
 import CourseMetaForm from "../../courses/_components/CourseMetaForm";
 import { useCurrentUser } from "@/hooks/use-current-user";
 import { usePermissions } from "@/hooks/use-permission";
@@ -609,6 +610,15 @@ export default function CourseManagementPage() {
             }}
           />
         </div>
+      )}
+
+      {activeTab === "settings" && currentCourse && (
+        <CollaboratorsPanel
+          courseId={courseId}
+          createdBy={currentCourse.created_by}
+          callerId={callerId}
+          callerRole={roleCode}
+        />
       )}
 
       {selectedStudentId && (
